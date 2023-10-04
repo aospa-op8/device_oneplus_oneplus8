33bca/android_device_oneplus_oneplus8

op8: Standardize oneplus pickup sensor
 * use standard sensor type
 * ignore non-wakeup sensor variant
 * ignore events that do not properly match a pickup

Change-Id: Icde499f10211bb88c3cdca199bf4944477835bfa

sensors/HalProxy.cpp
@@ -81,6 +81,25 @@ int64_t msFromNs(int64_t nanos) {
     return nanos / nanosecondsInAMillsecond;
 }
 
+bool patchXiaomiPickupSensor(V2_1::SensorInfo& sensor) {
+    if (sensor.typeAsString != "oneplus.sensor.op_motion_detect") {
+        return true;
+    }
+
+    /*
+     * Implement only the wake-up version of this sensor.
+     */
+    if (!(sensor.flags & V1_0::SensorFlagBits::WAKE_UP)) {
+        return false;
+    }
+
+    sensor.type = V2_1::SensorType::PICK_UP_GESTURE;
+    sensor.typeAsString = SENSOR_STRING_TYPE_PICK_UP_GESTURE;
+    sensor.maxRange = 1;
+
+    return true;
+}
+
 HalProxy::HalProxy() {
     const char* kMultiHalConfigFile = "/vendor/etc/sensors/hals.conf";
     initializeSubHalListFromConfigFile(kMultiHalConfigFile);
@@ -480,6 +499,11 @@ void HalProxy::initializeSensorList() {
                     ALOGV("Loaded sensor: %s", sensor.name.c_str());
                     sensor.sensorHandle = setSubHalIndex(sensor.sensorHandle, subHalIndex);
                     setDirectChannelFlags(&sensor, mSubHalList[subHalIndex]);
+                    bool keep = patchXiaomiPickupSensor(sensor);
+                    if (!keep) {
+                        continue;
+                    }
+
                     mSensors[sensor.sensorHandle] = sensor;
                 }
             }

aospa-op8/device_oneplus_oneplus8

sensors/HalProxy.cpp
@@ -81,6 +81,25 @@ int64_t msFromNs(int64_t nanos) {
     return nanos / nanosecondsInAMillsecond;
 }
 
+bool patchOnePlusPickupSensor(V2_1::SensorInfo& sensor) {
+    if (sensor.typeAsString != "oneplus.sensor.op_motion_detect") {
+        return true;
+    }
+
+    /*
+     * Implement only the wake-up version of this sensor.
+     */
+    if (!(sensor.flags & V1_0::SensorFlagBits::WAKE_UP)) {
+        return false;
+    }
+
+    sensor.type = V2_1::SensorType::PICK_UP_GESTURE;
+    sensor.typeAsString = SENSOR_STRING_TYPE_PICK_UP_GESTURE;
+    sensor.maxRange = 1;
+
+    return true;
+}
+
 HalProxy::HalProxy() {
     const char* kMultiHalConfigFile = "/vendor/etc/sensors/hals.conf";
     initializeSubHalListFromConfigFile(kMultiHalConfigFile);
@@ -480,6 +499,11 @@ void HalProxy::initializeSensorList() {
                     ALOGV("Loaded sensor: %s", sensor.name.c_str());
                     sensor.sensorHandle = setSubHalIndex(sensor.sensorHandle, subHalIndex);
                     setDirectChannelFlags(&sensor, mSubHalList[subHalIndex]);
+                    bool keep = patchOnePlusPickupSensor(sensor);
+                    if (!keep) {
+                        continue;
+                    }
+
                     mSensors[sensor.sensorHandle] = sensor;
                 }
             }